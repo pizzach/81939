c9dcd06a56d092d612159b09f512ceb6e5e21123
Rick Chen <rick_chen@asgard.com.tw>
2021-01-19 16:51:23 +0800

    first commit

---
+++ src/components/Title.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name:'title',
+  data(){
+    return {
+      title:'Hello Rick!'
+    }
+  }
+}
+</script>
+
+<style>
+  h1{
+    color:yellow;
+  }
+</style>
